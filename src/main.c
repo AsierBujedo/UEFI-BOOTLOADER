37b7e5406550df60c613da139ed3e38ddc4fad2c
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2024-11-15 23:39:03 +0000

    First commit

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
-#include <efi.h>
-#include <efilib.h>
+#include <Uefi.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello, UEFI!\n");
+EFI_STATUS
+EFIAPI
+UefiMain(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable
+) {
+    Print(L"Hello, UEFI World from CMake!\n");
     return EFI_SUCCESS;
 }
